732583637e7d483872ab912ec1a139272be64aba
Alina <lina.azarova.00@bk.ru>
2025-01-03 06:00:55 +0700

    WindowsFormsSearchForText

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace WindowsFormsSearchForText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxOriginal = new System.Windows.Forms.PictureBox();
+            this.pictureBoxProcessed = new System.Windows.Forms.PictureBox();
+            this.btnStartVideo = new System.Windows.Forms.Button();
+            this.btnFoundText = new System.Windows.Forms.Button();
+            this.btnOpenImage = new System.Windows.Forms.Button();
+            this.btnDetectTextFromSelection = new System.Windows.Forms.Button();
+            this.btnStopVideo = new System.Windows.Forms.Button();
+            this.textBoxDetectedText = new System.Windows.Forms.TextBox();
+            this.btnSaveImage = new System.Windows.Forms.Button();
+            this.btnApplyMask = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProcessed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxOriginal
+            // 
+            this.pictureBoxOriginal.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBoxOriginal.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxOriginal.Name = "pictureBoxOriginal";
+            this.pictureBoxOriginal.Size = new System.Drawing.Size(320, 207);
+            this.pictureBoxOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxOriginal.TabIndex = 0;
+            this.pictureBoxOriginal.TabStop = false;
+            // 
+            // pictureBoxProcessed
+            // 
+            this.pictureBoxProcessed.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBoxProcessed.Location = new System.Drawing.Point(12, 231);
+            this.pictureBoxProcessed.Name = "pictureBoxProcessed";
+            this.pictureBoxProcessed.Size = new System.Drawing.Size(320, 207);
+            this.pictureBoxProcessed.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxProcessed.TabIndex = 1;
+            this.pictureBoxProcessed.TabStop = false;
+            // 
+            // btnStartVideo
+            // 
+            this.btnStartVideo.Location = new System.Drawing.Point(595, 12);
+            this.btnStartVideo.Name = "btnStartVideo";
+            this.btnStartVideo.Size = new System.Drawing.Size(172, 61);
+            this.btnStartVideo.TabIndex = 2;
+            this.btnStartVideo.Text = "Запуск видео";
+            this.btnStartVideo.UseVisualStyleBackColor = true;
+            this.btnStartVideo.Click += new System.EventHandler(this.btnStartVideo_Click);
+            // 
+            // btnFoundText
+            // 
+            this.btnFoundText.Location = new System.Drawing.Point(418, 79);
+            this.btnFoundText.Name = "btnFoundText";
+            this.btnFoundText.Size = new System.Drawing.Size(172, 61);
+            this.btnFoundText.TabIndex = 4;
+            this.btnFoundText.Text = "Найти текст";
+            this.btnFoundText.UseVisualStyleBackColor = true;
+            this.btnFoundText.Click += new System.EventHandler(this.btnFoundText_Click);
+            // 
+            // btnOpenImage
+            // 
+            this.btnOpenImage.Location = new System.Drawing.Point(418, 12);
+            this.btnOpenImage.Name = "btnOpenImage";
+            this.btnOpenImage.Size = new System.Drawing.Size(172, 61);
+            this.btnOpenImage.TabIndex = 5;
+            this.btnOpenImage.Text = "Открыть изображение";
+            this.btnOpenImage.UseVisualStyleBackColor = true;
+            this.btnOpenImage.Click += new System.EventHandler(this.btnOpenImage_Click);
+            // 
+            // btnDetectTextFromSelection
+            // 
+            this.btnDetectTextFromSelection.Location = new System.Drawing.Point(418, 146);
+            this.btnDetectTextFromSelection.Name = "btnDetectTextFromSelection";
+            this.btnDetectTextFromSelection.Size = new System.Drawing.Size(172, 61);
+            this.btnDetectTextFromSelection.TabIndex = 7;
+            this.btnDetectTextFromSelection.Text = "Отобразить текст выбранного участка";
+            this.btnDetectTextFromSelection.UseVisualStyleBackColor = true;
+            this.btnDetectTextFromSelection.Click += new System.EventHandler(this.btnDetectTextFromSelection_Click);
+            // 
+            // btnStopVideo
+            // 
+            this.btnStopVideo.Location = new System.Drawing.Point(595, 79);
+            this.btnStopVideo.Name = "btnStopVideo";
+            this.btnStopVideo.Size = new System.Drawing.Size(172, 61);
+            this.btnStopVideo.TabIndex = 8;
+            this.btnStopVideo.Text = "Стоп видео";
+            this.btnStopVideo.UseVisualStyleBackColor = true;
+            this.btnStopVideo.Click += new System.EventHandler(this.btnStopVideo_Click);
+            // 
+            // textBoxDetectedText
+            // 
+            this.textBoxDetectedText.Location = new System.Drawing.Point(418, 319);
+            this.textBoxDetectedText.Multiline = true;
+            this.textBoxDetectedText.Name = "textBoxDetectedText";
+            this.textBoxDetectedText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxDetectedText.Size = new System.Drawing.Size(349, 119);
+            this.textBoxDetectedText.TabIndex = 9;
+            // 
+            // btnSaveImage
+            // 
+            this.btnSaveImage.Location = new System.Drawing.Point(595, 146);
+            this.btnSaveImage.Name = "btnSaveImage";
+            this.btnSaveImage.Size = new System.Drawing.Size(172, 61);
+            this.btnSaveImage.TabIndex = 10;
+            this.btnSaveImage.Text = "Сохранить";
+            this.btnSaveImage.UseVisualStyleBackColor = true;
+            this.btnSaveImage.Click += new System.EventHandler(this.btnSaveImage_Click);
+            // 
+            // btnApplyMask
+            // 
+            this.btnApplyMask.Location = new System.Drawing.Point(595, 213);
+            this.btnApplyMask.Name = "btnApplyMask";
+            this.btnApplyMask.Size = new System.Drawing.Size(172, 61);
+            this.btnApplyMask.TabIndex = 11;
+            this.btnApplyMask.Text = "Наложить маску";
+            this.btnApplyMask.UseVisualStyleBackColor = true;
+            this.btnApplyMask.Click += new System.EventHandler(this.btnApplyMask_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnApplyMask);
+            this.Controls.Add(this.btnSaveImage);
+            this.Controls.Add(this.textBoxDetectedText);
+            this.Controls.Add(this.btnStopVideo);
+            this.Controls.Add(this.btnDetectTextFromSelection);
+            this.Controls.Add(this.btnOpenImage);
+            this.Controls.Add(this.btnFoundText);
+            this.Controls.Add(this.btnStartVideo);
+            this.Controls.Add(this.pictureBoxProcessed);
+            this.Controls.Add(this.pictureBoxOriginal);
+            this.Name = "Form1";
+            this.Text = "Редактор №3";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProcessed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxOriginal;
+        private System.Windows.Forms.PictureBox pictureBoxProcessed;
+        private System.Windows.Forms.Button btnStartVideo;
+        private System.Windows.Forms.Button btnFoundText;
+        private System.Windows.Forms.Button btnOpenImage;
+        private System.Windows.Forms.Button btnDetectTextFromSelection;
+        private System.Windows.Forms.Button btnStopVideo;
+        private System.Windows.Forms.TextBox textBoxDetectedText;
+        private System.Windows.Forms.Button btnSaveImage;
+        private System.Windows.Forms.Button btnApplyMask;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.OCR;
+
+namespace WindowsFormsSearchForText
+{
+    public partial class Form1 : Form
+    {
+        private Image<Bgr, byte> originalImage;
+        private Image<Bgr, byte> processedImage;
+
+        private VideoCapture videoCapture;
+        private Timer playbackTimer;
+        private Emgu.CV.OCR.Tesseract ocr;
+
+        private CascadeClassifier faceDetector;
+
+        private Rectangle selectionRect; // Прямоугольник для выделения
+        private bool isSelecting = false; // Флаг для отслеживания, идет ли процесс выделения
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string tessdataPath = @"D:\WF_lab3\WindowsFormsSearchForText\bin\Debug\tessdata";
+            // Инициализация Tesseract 
+            ocr = new Emgu.CV.OCR.Tesseract(tessdataPath, "eng", OcrEngineMode.Default);
+
+            // Инициализация таймера для воспроизведения
+            playbackTimer = new Timer();
+            playbackTimer.Interval = 1000 / 30;
+
+
+            // Загружаем классификатор для лиц
+            faceDetector = new CascadeClassifier(@"D:\WF_lab3\WindowsFormsSearchForText\bin\Debug\haarcascade_frontalface_default.xml");
+
+
+            pictureBoxOriginal.MouseDown += pictureBoxOriginal_MouseDown;
+            pictureBoxOriginal.MouseMove += pictureBoxOriginal_MouseMove;
+            pictureBoxOriginal.MouseUp += pictureBoxOriginal_MouseUp;
+            pictureBoxOriginal.Paint += pictureBoxOriginal_Paint; 
+
+        }
+
+        private void btnOpenImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp",
+                Title = "Выберите изображение"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                originalImage = new Image<Bgr, byte>(openFileDialog.FileName);
+                processedImage = originalImage.Clone(); 
+
+                
+                pictureBoxOriginal.Image = originalImage.ToBitmap();
+            }
+        }
+
+        private void btnFoundText_Click(object sender, EventArgs e)
+        {
+            if (originalImage != null)
+            {
+                // Преобразуем изображение в градации серого
+                var gray = originalImage.Convert<Gray, byte>();
+
+                // Применяем бинаризацию с порогом Otsu
+                var binary = new Mat();
+                CvInvoke.Threshold(gray, binary, 0, 255, ThresholdType.Binary | ThresholdType.Otsu);
+
+                // Передаем бинаризованное изображение в OCR для распознавания текста
+                ocr.SetImage(binary);
+                ocr.Recognize();
+
+                var resultImage = originalImage.Clone();
+
+                // Получаем текстовые области
+                var words = ocr.GetCharacters();
+
+                foreach (var word in words)
+                {
+                    // Получаем координаты каждого слова
+                    var rect = word.Region;
+
+                    // Рисуем прямоугольник вокруг слова
+                    CvInvoke.Rectangle(resultImage, rect, new MCvScalar(0, 255, 0), 2);
+                }
+
+                // Отображаем результат
+                processedImage = resultImage;
+                pictureBoxProcessed.Image = processedImage.ToBitmap();
+            }
+            else
+            {
+                MessageBox.Show("Сначала загрузите изображение!");
+            }
+        }
+
+        // Обработчик для рисования прямоугольника на изображении
+        private void pictureBoxOriginal_Paint(object sender, PaintEventArgs e)
+        {
+            if (isSelecting && selectionRect.Width > 0 && selectionRect.Height > 0)
+            {
+                // Рисуем временную рамку на изображении с учетом масштаба PictureBox
+                var scaleX = (float)originalImage.Width / pictureBoxOriginal.Width;
+                var scaleY = (float)originalImage.Height / pictureBoxOriginal.Height;
+
+                // Масштабируем координаты рамки для правильного отображения
+                var scaledRect = new Rectangle(
+                    (int)(selectionRect.X / scaleX),
+                    (int)(selectionRect.Y / scaleY),
+                    (int)(selectionRect.Width / scaleX),
+                    (int)(selectionRect.Height / scaleY)
+                );
+
+                e.Graphics.DrawRectangle(Pens.Red, scaledRect); // Рисуем красный прямоугольник
+            }
+        }
+
+        // Обработчик нажатия кнопки мыши
+        private void pictureBoxOriginal_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (originalImage != null)
+            {
+                // Начинаем выделение области с места нажатия
+                isSelecting = true;
+                selectionRect = new Rectangle(e.Location, new Size(0, 0)); // Устанавливаем начальную точку выделения
+            }
+        }
+
+        // Обработчик движения мыши
+        private void pictureBoxOriginal_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isSelecting && originalImage != null)
+            {
+                // Обновляем размер прямоугольника при движении мыши
+                selectionRect.Width = e.X - selectionRect.X;
+                selectionRect.Height = e.Y - selectionRect.Y;
+
+                pictureBoxOriginal.Invalidate(); // Перерисовываем PictureBox для отображения рамки
+
+                // Обновляем отображение выбранного фрагмента в pictureBoxProcessed в реальном времени
+                if (selectionRect.Width > 0 && selectionRect.Height > 0)
+                {
+                    // Создаем подпрямоугольник для отображения
+                    var selectedRegion = originalImage.GetSubRect(selectionRect);
+                    processedImage = selectedRegion.Clone(); // Клонируем выбранную область
+
+                    // Обновляем pictureBoxProcessed
+                    pictureBoxProcessed.Image = processedImage.ToBitmap();
+                }
+            }
+        }
+
+        // Обработчик отпускания кнопки мыши
+        private void pictureBoxOriginal_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (isSelecting && originalImage != null)
+            {
+                // Завершаем процесс выделения
+                isSelecting = false;
+
+                // Если прямоугольник выделен, отображаем выбранную область
+                if (selectionRect.Width > 0 && selectionRect.Height > 0)
+                {
+                    // Вырезаем выбранную область
+                    var selectedRegion = originalImage.GetSubRect(selectionRect);
+                    processedImage = selectedRegion.Clone(); // Клонируем выбранную область
+
+                    // Отображаем выбранную область в pictureBoxProcessed
+                    pictureBoxProcessed.Image = processedImage.ToBitmap();
+                }
+            }
+        }
+
+        private void btnDetectTextFromSelection_Click(object sender, EventArgs e)
+        {
+            if (selectionRect.Width > 0 && selectionRect.Height > 0 && originalImage != null)
+            {
+                // Извлекаем выбранную область изображения
+                var selectedRegion = originalImage.GetSubRect(selectionRect);
+
+                // Преобразуем выделенную область в изображение в градациях серого
+                var gray = selectedRegion.Convert<Gray, byte>();
+
+                // Применяем бинаризацию
+                var binary = new Mat();
+                CvInvoke.Threshold(gray, binary, 0, 255, ThresholdType.Otsu);
+
+                // Распознаем текст с помощью Tesseract
+                ocr.SetImage(binary);
+                ocr.Recognize();
+
+                // Получаем распознанный текст
+                string detectedText = ocr.GetUTF8Text();
+
+                // Отображаем текст в TextBox
+                textBoxDetectedText.Text = detectedText;
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, выделите область для распознавания текста!");
+            }
+        }
+
+        private void btnStartVideo_Click(object sender, EventArgs e)
+        {
+            if (videoCapture == null)
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog
+                {
+                    Filter = "Video Files|*.mp4;*.avi;*.mov;*.mkv",
+                    Title = "Выберите видеофайл или используйте камеру"
+                };
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        videoCapture = new VideoCapture(openFileDialog.FileName);
+                        MessageBox.Show("Видео успешно загружено!");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Ошибка при загрузке видео: " + ex.Message);
+                        return;
+                    }
+                }
+                else
+                {
+                    // Если пользователь не выбрал файл, использовать камеру
+                    videoCapture = new VideoCapture();
+                    MessageBox.Show("Камера используется по умолчанию.");
+                }
+
+                Application.Idle += ProcessVideoFrame;
+            }
+            else
+            {
+                MessageBox.Show("Видео уже запущено.");
+            }
+        }
+
+        private void ProcessVideoFrame(object sender, EventArgs e)
+        {
+            var frame = new Mat();
+            videoCapture.Read(frame);
+
+            if (!frame.IsEmpty)
+            {
+                var grayFrame = new Mat();
+                CvInvoke.CvtColor(frame, grayFrame, ColorConversion.Bgr2Gray);
+
+                // Детекция лиц
+                var faces = faceDetector.DetectMultiScale(grayFrame, 1.1, 4, new Size (30, 30), new Size(100, 100));
+                foreach (var face in faces)
+                {
+                    CvInvoke.Rectangle(frame, face, new MCvScalar(255, 0, 0), 2); // Рисуем прямоугольник вокруг лица
+                }
+
+                // Отображение кадра с лицами в pictureBoxOriginal
+                pictureBoxOriginal.Image = frame.ToBitmap();
+
+                // Выполнение OCR
+                string detectedText = PerformOCR(frame);
+                if (!string.IsNullOrEmpty(detectedText))
+                {
+                    textBoxDetectedText.AppendText(detectedText + Environment.NewLine);
+                }
+            }
+        }
+
+        private void btnStopVideo_Click(object sender, EventArgs e)
+        {
+            if (videoCapture != null)
+            {
+                var frame = new Mat();
+                videoCapture.Read(frame);
+
+                if (!frame.IsEmpty)
+                {
+                    processedImage = frame.ToImage<Bgr, byte>();
+
+                    // Преобразуем кадр в оттенки серого для детекции лиц
+                    var grayFrame = new Mat();
+                    CvInvoke.CvtColor(processedImage, grayFrame, ColorConversion.Bgr2Gray);
+
+                    // Детекция лиц
+                    var faces = faceDetector.DetectMultiScale(grayFrame, 1.1, 4, Size.Empty, Size.Empty);
+                    foreach (var face in faces)
+                    {
+                        // Рисуем прямоугольники вокруг лиц
+                        CvInvoke.Rectangle(processedImage, face, new MCvScalar(0, 0, 255), 2); // Красные прямоугольники
+                    }
+
+                    pictureBoxProcessed.Image = processedImage.ToBitmap();
+                }
+
+                Application.Idle -= ProcessVideoFrame;
+                videoCapture.Dispose();
+                videoCapture = null;
+            }
+        }
+        private string PerformOCR(Mat frame)
+        {
+            try
+            {
+                var grayFrame = new Mat();
+                CvInvoke.CvtColor(frame, grayFrame, ColorConversion.Bgr2Gray);
+
+                // OCR обработка
+                ocr.SetImage(grayFrame);
+                ocr.Recognize();
+                return ocr.GetUTF8Text();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка OCR: " + ex.Message);
+                return string.Empty;
+            }
+        }
+
+        private void btnSaveImage_Click(object sender, EventArgs e)
+        {
+            if (processedImage != null)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog
+                {
+                    Filter = "PNG Image|*.png",
+                    Title = "Сохранить обработанное изображение"
+                };
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    processedImage.Save(saveFileDialog.FileName);
+                    MessageBox.Show("Изображение успешно сохранено!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Сначала обработайте изображение!");
+            }
+        }
+
+        private void btnApplyMask_Click(object sender, EventArgs e)
+        {
+            if (processedImage != null)
+            {
+                var grayImage = processedImage.Convert<Gray, byte>();
+                var faces = faceDetector.DetectMultiScale(grayImage, 1.1, 4, Size.Empty, Size.Empty);
+
+                foreach (var face in faces)
+                {
+                    CvInvoke.Rectangle(processedImage, face, new MCvScalar(0, 0, 255), -1); // Наложение маски (красный прямоугольник)
+                }
+
+                pictureBoxProcessed.Image = processedImage.ToBitmap();
+            }
+            else
+            {
+                MessageBox.Show("Сначала обработайте изображение!");
+            }
+        }
+    }
+}
